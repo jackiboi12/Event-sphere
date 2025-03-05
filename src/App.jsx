fa91618f2ba7d3785d58f51c9381f1b699dc29bc
jackiboi12 <satyamjakhmola99@gmail.com>
2025-03-05 12:53:04 +0530

    create pages template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/sign-in" element={<SignIn/>}></Route>
+      <Route path="/sign-up" element={<SignUp/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
